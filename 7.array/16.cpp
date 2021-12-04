e14c90a632d10ba923a689ae420410a939ba5ecd
Acorn272 <maitran.mt02@gmail.com>
2021-12-04 11:52:36 -0600

    The source code we need but not we deserve'

--- 7.array/16.cpp
+++ 7.array/16.cpp
@@ -14,11 +14,72 @@ the program should:
 #include <vector>
 using namespace std;
 
+const string nameFile = "Teams.txt";
+const string winnerFile = "WorldSeriesWinners.txt";
+
 // To Do: retrieve data from Teams.txt(should take 2 parameter, file name and string vector name)
+bool getList(vector<string> &teamsList, string fileName) {
+	
+	ifstream inputFile(fileName);
+	
+	if (!inputFile) {
+		cout << "Error opening file named " << fileName << "\n";
+		return false;
+	} else {
+		cout << "File open successfully\n";
+		string Name;
+		int count = 0;
+	  while (!inputFile.eof()) {
+			getline(inputFile, Name);
+		  teamsList.push_back(Name);
+			count++; 
+	  }
+		cout << count << " items has been successfully read from " << fileName << "\n";
+	  inputFile.close();
+		return true;
+	}
+
+}
 
 // To Do: display list of Teams(take string vector as parameter)
+void displayList(const vector<string> teamsList) {
+	for (int index = 0; index < teamsList.size(); index++ ) {
+		cout << index << ". " << teamsList[index] << "\n";
+	}
+}
+
 
 // To Do: find number of time that Team has Won(take name vector parameter and year int vector)
+int getNumberOfWinTime(const vector<string> teamsList, string fileName) {
+	
+	cout << "\nPlease enter Team's Name or index: ";
+	string name;
+	int index = 0;
+  
+	getline(cin, name);
+	if (name.size() == 1 || name.size() == 2) {
+		index = stoi(name);
+		cout << "Index input received\n";
+		name = teamsList[index];
+	}
+	
+	cout << "You entered: " << name << endl;
+	
+	ifstream inputFile(fileName);
+	if (!inputFile) {
+		cout << "Error opening file name " << fileName << "\n";
+	} else {
+		int count = 0;
+		while (!inputFile.eof()) {
+			string dataLine;
+			getline(inputFile, dataLine);
+			if (dataLine == name) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
 
 // To Do: display the year that team has won(take string vector parameter)
 
@@ -27,8 +88,21 @@ int main() {
 	// string vector than contain name of several Leagut basball teams
 	vector<string> TeamsList;
 	
-	// call retrieve data vector
+	// call retrieve data vector , if true then call display function
+	if (getList(TeamsList, nameFile)) {
+		displayList(TeamsList);
+		
+		int winTimes;
+		winTimes = getNumberOfWinTime(TeamsList, winnerFile);
+		cout << endl;
+		if (winTimes != 0) {
+		  cout << "The team you entered has won " << winTimes << " times\n";
+		} else {
+			cout << "The team you entered is not match\n";
+		}
+	}
 	
-	// call display 
-	
+	// notify and terminate program
+	cout << "\nDone!\n";
+	return 0;
 }
