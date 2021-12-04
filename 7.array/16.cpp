764ea2e81978eb24a2400bdbd0bc816705caf476
Acorn27 <maitran.mt02@gmail.com>
2021-12-04 08:21:31 -0600

    Done

--- 7.array/16.cpp
+++ 7.array/16.cpp
@@ -1,9 +1,9 @@
 /*
 This program need two .txt files: Teams.txt and WorldSeriesWinners.txt
-Teams.txt contains a list of everal Major League baseball teams in alphabest order
+Teams.txt contains a list of several Major League baseball teams in alphabest order
 WorldSeriesWinners constain chronogical list of these Team from 1903 to 2021
 the program should:
-  + displat a list of team retrieve from data in Teams.txt
+    + display a list of team retrieve from data in Teams.txt
 	+ Allow user to enter name of basball team they want to find year window
 	+ Display that team winning year as well as number of time they has won the World Series
 */
@@ -11,18 +11,24 @@ the program should:
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-// To Do: retrieve data from Teams.txt
+// To Do: retrieve data from Teams.txt(should take 2 parameter, file name and string vector name)
 
-// To Do: display list of Teams
+// To Do: display list of Teams(take string vector as parameter)
 
-// To Do: find number of time that Team has Won
+// To Do: find number of time that Team has Won(take name vector parameter and year int vector)
 
-// To Do: display the year that team has won 
+// To Do: display the year that team has won(take string vector parameter)
 
 int main() {
 	
-	// To Do 
+	// string vector than contain name of several Leagut basball teams
+	vector<string> TeamsList;
+	
+	// call retrieve data vector
+	
+	// call display 
 	
 }
